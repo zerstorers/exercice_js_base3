da7510e3bd5f5cf2d5dd69f0f94bb867c2a8b18c
zerstorers <lemaire-maxime@hotmail.fr>
2019-09-16 13:50:36 +0200

    exo

--- exercice 2/script.js
+++ exercice 2/script.js
@@ -1,7 +1,17 @@
+
+
 var text = document.getElementById("texte");
 var btnpop = document.getElementsByTagName("a")[0];
 var btnend = document.getElementsByTagName("a")[1];
 
-btnend.addEventListener("click" , function(){
-    text.parentNode.removeChild(text);
+// btnend.addEventListener("click" , function(){
+//     text.parentNode.removeChild(text);
+// });
+
+btnend.addEventListener("click", function(){
+    text.style.display = "none";
 });
+
+btnpop.addEventListener("click", function(){
+    text.style.display = "block";
+})
